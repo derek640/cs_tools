5bb1dfc1d038cbbbf9486ff88b729113b4a86800
程顺 <chengshun@corp.netease.com>
2020-01-24 16:48:24 +0800

    modify

--- scripts/render/beauty_ao_shadow_layerManager.py
+++ scripts/render/beauty_ao_shadow_layerManager.py
@@ -1,64 +1,64 @@
 import os, sys
-from pymel.core import *
 import maya.app.renderSetup as renderSetup
 import mtoa.aovs as aovs
 
 
-if not objExists('objs'):
-    confirmDialog(t='confirm', m='No objs grp, please put all object under it!', b=['ok'])
-    print 'haha'
-    sys.exit('')
+def create_layers():
+    if not objExists('objs'):
+        confirmDialog(t='confirm', m='No objs grp, please put all object under it!', b=['ok'])
+        print 'haha'
+        sys.exit('')
 
-rs = renderSetup.model.renderSetup.instance()
-rs.getDefaultRenderLayer().setRenderable(0)
+    rs = renderSetup.model.renderSetup.instance()
+    rs.getDefaultRenderLayer().setRenderable(0)
 
-aovs.AOVInterface().addAOV('ao', aovType='float')
-ao = createNode('aiAmbientOcclusion')
-ao.outColor >> PyNode('aiAOV_ao').defaultValue
+    aovs.AOVInterface().addAOV('ao', aovType='float')
+    ao = createNode('aiAmbientOcclusion')
+    ao.outColor >> PyNode('aiAOV_ao').defaultValue
 
-asm = createNode('aiShadowMatte')
+    asm = createNode('aiShadowMatte')
 
-# rs layer
-for obj in PyNode('objs').getChildren():
+    # rs layer
+    for obj in PyNode('objs').getChildren():
 
-    # beauty with out shadow
-    print obj.name(), obj.type()
+        # beauty with out shadow
+        print obj.name(), obj.type()
 
-    obj.getShape().aiSubdivType.set(1)
-    obj.getShape().aiSubdivIterations.set(2)
+        obj.getShape().aiSubdivType.set(1)
+        obj.getShape().aiSubdivIterations.set(2)
 
-    rsl = rs.createRenderLayer('rsl_beauty_'+obj.name())
-    col = rsl.createCollection('ca1_beauty_'+obj.name())
-    col.getSelector().setPattern('objs')
-    cola = col.createCollection('ca2_beauty_'+obj.name())
-    cola.getSelector().setFilterType(2)
-    cola.getSelector().setPattern('* -'+obj.getShape().name())
-    ov_matte = cola.createOverride('override_beauty_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
-    ov_matte.finalize('aiMatte')
-    ov_matte.setAttrValue(1)
-    ov_shadow = cola.createOverride('override_beauty_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
-    ov_shadow.finalize('castsShadows')
-    ov_shadow.setAttrValue(0)
+        rsl = rs.createRenderLayer('rsl_beauty_'+obj.name())
+        col = rsl.createCollection('ca1_beauty_'+obj.name())
+        col.getSelector().setPattern('objs')
+        cola = col.createCollection('ca2_beauty_'+obj.name())
+        cola.getSelector().setFilterType(2)
+        cola.getSelector().setPattern('* -'+obj.getShape().name())
+        ov_matte = cola.createOverride('override_beauty_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_matte.finalize('aiMatte')
+        ov_matte.setAttrValue(1)
+        ov_shadow = cola.createOverride('override_beauty_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_shadow.finalize('castsShadows')
+        ov_shadow.setAttrValue(0)
 
-    # shadow
-    rsl = rs.createRenderLayer('rsl_shadow_'+obj.name())
-    col = rsl.createCollection('ca1_shadow_'+obj.name())
-    col.getSelector().setPattern('objs')
-    cola = col.createCollection('ca2_shadow_'+obj.name())
-    cola.getSelector().setFilterType(2)
-    cola.getSelector().setPattern('* -'+obj.getShape().name())
-    ov_matte = cola.createOverride('override_shadow_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
-    ov_matte.finalize('aiMatte')
-    ov_matte.setAttrValue(1)
-    ov_shadow = cola.createOverride('override_shadow_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
-    ov_shadow.finalize('primaryVisibility')
-    ov_shadow.setAttrValue(0)
+        # shadow
+        rsl = rs.createRenderLayer('rsl_shadow_'+obj.name())
+        col = rsl.createCollection('ca1_shadow_'+obj.name())
+        col.getSelector().setPattern('objs')
+        cola = col.createCollection('ca2_shadow_'+obj.name())
+        cola.getSelector().setFilterType(2)
+        cola.getSelector().setPattern('* -'+obj.getShape().name())
+        ov_matte = cola.createOverride('override_shadow_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_matte.finalize('aiMatte')
+        ov_matte.setAttrValue(1)
+        ov_shadow = cola.createOverride('override_shadow_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_shadow.finalize('primaryVisibility')
+        ov_shadow.setAttrValue(0)
 
-    cola = col.createCollection('ca3_shadow_'+obj.name())
-    cola.getSelector().setFilterType(2)
-    cola.getSelector().setPattern(obj.getShape().name())
-    colb = cola.createCollection('ca4_shadow_'+obj.name())
-    colb.getSelector().setFilterType(5)
-    colb.getSelector().setPattern('*')
-    ov_shader = cola.createOverride('override2_beauty_s_'+obj.name(), renderSetup.model.typeIDs.shaderOverride)
-    ov_shader.setShader('aiShadowMatte1')
+        cola = col.createCollection('ca3_shadow_'+obj.name())
+        cola.getSelector().setFilterType(2)
+        cola.getSelector().setPattern(obj.getShape().name())
+        colb = cola.createCollection('ca4_shadow_'+obj.name())
+        colb.getSelector().setFilterType(5)
+        colb.getSelector().setPattern('*')
+        ov_shader = cola.createOverride('override2_beauty_s_'+obj.name(), renderSetup.model.typeIDs.shaderOverride)
+        ov_shader.setShader('aiShadowMatte1')
