6b5574f12bf60c202bb7be401c377ce04733c8ce
程顺 <chengshun@corp.netease.com>
2020-01-24 16:50:21 +0800

    fix

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 mkdir D:\cs_tools\scripts\render
-curl https://raw.githubusercontent.com/derek640/cs_tools/master/scripts/beauty_ao_shadow_layerManager.py -o D:\cs_tools\scripts\render\
+curl https://raw.githubusercontent.com/derek640/cs_tools/master/scripts/render/beauty_ao_shadow_layerManager.py -o D:\cs_tools\scripts\render\
 curl https://raw.githubusercontent.com/derek640/cs_tools/master/userSetup.py -o D:\cs_tools\scripts\
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PYTHONPATH`) do set my_user_pythonpath=%%B
 setx PYTHONPATH "D:\cs_tools\;%PYTHONPATH%"
