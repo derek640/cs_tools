3199bc0129a7fb813830b178346ad2ed857d95ea
程顺 <chengshun@corp.netease.com>
2020-02-07 21:38:49 +0800

    fix bug

--- scripts/render/beauty_ao_shadow_layerManager.py
+++ scripts/render/beauty_ao_shadow_layerManager.py
@@ -1,26 +1,29 @@
 import os, sys
-import maya.app.renderSetup as renderSetup
-import mtoa.aovs as aovs
-from pymel.core import *
+import pymel.core as pm
 
+import maya.app.renderSetup.model.renderSetup as rs
+from maya.app.renderSetup.model.typeIDs import *
+
+import mtoa.core as ar
+import mtoa.aovs as aovs
 
 def create_layers():
-    if not objExists('objs'):
-        confirmDialog(t='confirm', m='No objs grp, please put all object under it!', b=['ok'])
-        print 'haha'
+    if not pm.objExists('objs'):
+        pm.confirmDialog(t='confirm', m='No objs grp, please put all object under it!', b=['ok'])
         sys.exit('')
 
-    rs = renderSetup.model.renderSetup.instance()
-    rs.getDefaultRenderLayer().setRenderable(0)
+    rs_ins = rs.instance()
+    rs_ins.getDefaultRenderLayer().setRenderable(0)
+    ar.createOptions()
 
     aovs.AOVInterface().addAOV('ao', aovType='float')
-    ao = createNode('aiAmbientOcclusion')
-    ao.outColor >> PyNode('aiAOV_ao').defaultValue
+    ao = pm.createNode('aiAmbientOcclusion')
+    ao.outColor >> pm.PyNode('aiAOV_ao').defaultValue
 
-    asm = createNode('aiShadowMatte')
+    asm = pm.createNode('aiShadowMatte')
 
     # rs layer
-    for obj in PyNode('objs').getChildren():
+    for obj in pm.PyNode('objs').getChildren():
 
         # beauty with out shadow
         print obj.name(), obj.type()
@@ -28,30 +31,30 @@ def create_layers():
         obj.getShape().aiSubdivType.set(1)
         obj.getShape().aiSubdivIterations.set(2)
 
-        rsl = rs.createRenderLayer('rsl_beauty_'+obj.name())
+        rsl = rs_ins.createRenderLayer('rsl_beauty_'+obj.name())
         col = rsl.createCollection('ca1_beauty_'+obj.name())
         col.getSelector().setPattern('objs')
         cola = col.createCollection('ca2_beauty_'+obj.name())
         cola.getSelector().setFilterType(2)
         cola.getSelector().setPattern('* -'+obj.getShape().name())
-        ov_matte = cola.createOverride('override_beauty_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_matte = cola.createOverride('override_beauty_m_'+obj.name(), absOverride)
         ov_matte.finalize('aiMatte')
         ov_matte.setAttrValue(1)
-        ov_shadow = cola.createOverride('override_beauty_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_shadow = cola.createOverride('override_beauty_s_'+obj.name(), absOverride)
         ov_shadow.finalize('castsShadows')
         ov_shadow.setAttrValue(0)
 
         # shadow
-        rsl = rs.createRenderLayer('rsl_shadow_'+obj.name())
+        rsl = rs_ins.createRenderLayer('rsl_shadow_'+obj.name())
         col = rsl.createCollection('ca1_shadow_'+obj.name())
         col.getSelector().setPattern('objs')
         cola = col.createCollection('ca2_shadow_'+obj.name())
         cola.getSelector().setFilterType(2)
         cola.getSelector().setPattern('* -'+obj.getShape().name())
-        ov_matte = cola.createOverride('override_shadow_m_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_matte = cola.createOverride('override_shadow_m_'+obj.name(), absOverride)
         ov_matte.finalize('aiMatte')
         ov_matte.setAttrValue(1)
-        ov_shadow = cola.createOverride('override_shadow_s_'+obj.name(), renderSetup.model.typeIDs.absOverride)
+        ov_shadow = cola.createOverride('override_shadow_s_'+obj.name(), absOverride)
         ov_shadow.finalize('primaryVisibility')
         ov_shadow.setAttrValue(0)
 
@@ -61,5 +64,5 @@ def create_layers():
         colb = cola.createCollection('ca4_shadow_'+obj.name())
         colb.getSelector().setFilterType(5)
         colb.getSelector().setPattern('*')
-        ov_shader = cola.createOverride('override2_beauty_s_'+obj.name(), renderSetup.model.typeIDs.shaderOverride)
+        ov_shader = cola.createOverride('override2_beauty_s_'+obj.name(), shaderOverride)
         ov_shader.setShader('aiShadowMatte1')
