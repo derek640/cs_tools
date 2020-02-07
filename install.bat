b3727d512a8f4c09992c6c4fa373cae5bbfc9827
程顺 <chengshun@corp.netease.com>
2020-02-07 11:57:42 +0800

    add __init__.py add make this file to install.bat

--- install.bat
+++ install.bat
@@ -5,6 +5,9 @@ mkdir D:\cs_tools\scripts\render
 curl https://raw.githubusercontent.com/derek640/cs_tools/master/scripts/render/beauty_ao_shadow_layerManager.py -o D:\cs_tools\scripts\render\beauty_ao_shadow_layerManager.py
 curl https://raw.githubusercontent.com/derek640/cs_tools/master/userSetup.py -o D:\cs_tools\userSetup.py
 
+copy /y NUL D:\cs_tools\scripts\__init__.py
+copy /y NUL D:\cs_tools\scripts\render\__init__.py
+
 if defined PYTHONPATH (
     for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PYTHONPATH`) do set my_user_pythonpath=%%B
     goto a
